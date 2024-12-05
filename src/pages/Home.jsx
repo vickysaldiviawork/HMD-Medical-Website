5ae84f52fe80bfd975b2a66d9898e8df9157b775
victoria saldivia <vsaldivia.work@gmail.com>
2024-12-05 18:09:04 -0400

    FEAT- WhatsappPopUp ✨

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import WhatsappPopUp from "../components/WhatsappPopUp";
+
 const Home = () => {
-    return (
+    return <>
         <div>
             <h1>Bienvenido a la Página Principal</h1>
             <p>Contenido de la página de inicio.</p>
         </div>
-    );
+
+        <div>
+            <WhatsappPopUp />
+        </div>
+    </>
 };
 
 export default Home;
