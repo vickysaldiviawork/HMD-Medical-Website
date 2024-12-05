a66b617fbea07dcc172e6adac86389a3422cb032
victoria saldivia <vsaldivia.work@gmail.com>
2024-12-05 14:02:30 -0400

    Footer Updated ✨
    Initial Structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,44 @@
+import "../global.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+
 const Footer = () => {
     return (
-        <div>
-            <h1>SOY EL FOOTER</h1>
-        </div>
+        <footer className="footerFooter">
+            <div className="topFooter">
+                <img src="" alt="logoHMDMedical" />
+                <p className="infoHMD">
+                    Somos una empresa dedicada a la fabricación, distribución y comercialización de productos médicos en Venezuela.
+                </p>
+            </div>
+
+            <div className="containerFooter">
+                <div className="generalFooter">
+                    <h4>¿Interesado en algún producto?</h4>
+                    <h3>¡Haz tu pedido ahora!</h3>
+                    <button className="botonPedidoFooter">Contáctanos</button>
+                </div>
+
+                <div className="paginasFooter">
+                    <h3>Navegación</h3>
+                    <ul>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/contact">Contacto</a></li>
+                        <li><a href="/us">Nosotros</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="bottomFooter">
+                <div className="emailInfo">
+                    <AiOutlineMail /> <span>hmdventas@hmdmedical.net</span>
+                </div>
+
+                <div className="instagramInfo">
+                    <FaInstagram /> <a href="https://www.instagram.com/hmdmedicalvzla/">HMD Medical Vzla</a>
+                </div>
+            </div>
+        </footer>
     );
 }
 
