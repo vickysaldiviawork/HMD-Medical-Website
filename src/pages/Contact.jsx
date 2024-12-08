f8f8683788a39e79f32184606807b197677959c4
victoria saldivia <vsaldivia.work@gmail.com>
2024-12-08 12:51:01 -0400

    Update Responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,30 +16,40 @@ const Contact = () => {
 
                     <div className="info-item" onClick={openWhatsApp}>
                         <FaPhone className="contact-icon" />
-                        <a><b>Teléfono:</b> +58 424 227 3484</a>
+                        <a className='telfContact'><b>Teléfono:</b> +58 424 227 3484</a>
                     </div>
 
                     <div className="info-item">
                         <FaEnvelope className="contact-icon" />
-                        <span><b>Email:</b> contacto@hmdmedical.com</span>
+                        <span className='emailContact'><b>Email:</b> hmdventas@hmdmedica.net</span>
                     </div>
 
                     <div className="info-item">
                         <FaMapMarkerAlt className="contact-icon" />
-                        <span><b>Dirección:</b> Guatire, Estado Miranda, Venezuela.</span>
+                        <span className='direccContact'><b>Dirección:</b> Guatire, Estado Miranda, Venezuela.</span>
                     </div>
 
-                    <h2 className='redestitle'>Redes Sociales</h2>
-                    <p>¡Encuéntranos en las Redes!</p>
-                    
-                    <div className="instagramInfoContact">
-                        <FaInstagram /> <a href="https://www.instagram.com/hmdmedicalvzla/">HMD Medical Vzla</a>
-                    </div>
                 </div>
 
+                <div className='contactInfo'>
+                    <div className='adicionalContact'>
+                        <div>
+                            <h2>Redes Sociales</h2>
+                            <p>¡Encuéntranos en las Redes!</p>
+                            <div className="instagramInfoContact">
+                                <FaInstagram /> <a href="https://www.instagram.com/hmdmedicalvzla/">HMD Medical Vzla</a>
+                            </div>
+                        </div>
 
-                <div className="nosotrosInfo">
+                        <div>
+                            <h2>Conócenos</h2>
+                            <p>Descubre <b>quiénes somos</b>, nuestra historia y lo que nos inspira a seguir creciendo:</p>
+                            <div className="instagramInfoContact">
+                                <a href="/us">Nosotros</a>
+                            </div>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </>
