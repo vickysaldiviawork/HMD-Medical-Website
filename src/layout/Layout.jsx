502aded23c9fb582323eddb60168734a091c0392
victoria saldivia <vsaldivia.work@gmail.com>
2025-04-01 16:04:52 -0400

    ScrollToTop

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const Layout = () => {
-    return (
-        <>
-            <NavBar />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </>
-    );
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Desplazamiento suave al principio
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    
+    // Fallback para navegadores que no soportan 'smooth'
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
+  }, [pathname]);
+
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
